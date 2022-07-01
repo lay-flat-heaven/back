d2cfd75403abce00323ba3587849410fce21d230
shinoshina <1982600638@qq.com>
2022-07-01 10:50:39 +0800

    这只是个测试端，加了跨域过滤

--- test.go
+++ test.go
@@ -6,27 +6,53 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CORS() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		// 允许 Origin 字段中的域发送请求
+		context.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		// 设置预验请求有效期为 86400 秒
+		context.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		// 设置允许请求的方法
+		context.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE, PATCH")
+		// 设置允许请求的 Header
+		context.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length，Apitoken")
+		// 设置拿到除基本字段外的其他字段，如上面的Apitoken, 这里通过引用Access-Control-Expose-Headers，进行配置，效果是一样的。
+		context.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Headers")
+		// 配置是否可以带认证信息
+		context.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		// OPTIONS请求返回200
+		if context.Request.Method == "OPTIONS" {
+			fmt.Println(context.Request.Header)
+			context.AbortWithStatus(200)
+		} else {
+			context.Next()
+		}
+	}
+}
 func main() {
 
 	router := gin.Default()
+	router.Use(CORS())
 
-	router.GET("/",func(c *gin.Context) {
-		c.JSON(202,gin.H{
-			"hello":"helllo",
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(202, gin.H{
+			"hello": "helllo",
 		})
 	})
 	router.POST("/upload", func(c *gin.Context) {
-	
-		file, _ := c.FormFile("file")
+		form, _ := c.MultipartForm()
 
-		fmt.Println(file.Filename)
+		files, _ := form.File["file"]
 
-		
-		 c.SaveUploadedFile(file, "/home/shinoshina/gocode/src/cback/kk.txt")
-		
+		for _, file := range files {
 
-		c.String(202, fmt.Sprintf("'%s' uploaded!", file.Filename))
+			c.SaveUploadedFile(file, file.Filename)
+
+		}
+
+		c.String(202, fmt.Sprintf("uploaded!"))
 	})
-	router.Run(":8080")
+	router.Run(":9099")
 
 }
+
